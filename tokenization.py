f0bd65c92b06914847e4f0bd7905aff9c0f3fcde
LynSChen04 <lynchen04@gmail.com>
2025-02-27 14:31:12 -0500

    tokenization file added that adds a column to every csv file in a given directory with a list of all the tokens

---
+++ tokenization.py
@@ -0,0 +1,25 @@
+from pygments.lexers.jvm import JavaLexer
+#from pygments.lexer import get_lexer_by_name
+from pygments.token import Token
+import os
+import pandas as pd
+
+lexer = JavaLexer()
+
+def tokenize_code(code):
+    tokens = [t[1] for t in lexer.get_tokens(code)]
+    return tokens
+
+def add_tokens_to_csv_files(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith(".csv"):
+            filepath = os.path.join(directory, filename)
+            df = pd.read_csv(filepath)
+            if "Method Code No Comments" in df.columns:
+                df["Tokens"] = df["Method Code No Comments"].apply(tokenize_code)                    
+            df.to_csv(filepath, index=False)
+
+
+# Example usage
+directory_path = 'data/'
+add_tokens_to_csv_files(directory_path)
